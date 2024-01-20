9f0e7eaf69ebb374669bb92153e14323b2b36930
HiagoSalvador <hiago.salvador@hotmail.com>
2024-01-20 11:26:18 -0300

    OrderStatus enum

--- src/main/java/com/estudo/course/resources/UserResource.java
+++ src/main/java/com/estudo/course/resources/UserResource.java
@@ -9,25 +9,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.estudo.course.entities.Order;
-import com.estudo.course.services.OrderService;
+
+import com.estudo.course.entities.User;
+import com.estudo.course.services.UserService;
 
 @RestController
-@RequestMapping(value = "/orders")
+@RequestMapping(value = "/users")
 public class UserResource {
 	@Autowired
-	private OrderService service;
+	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<Order>> findAll() {
-		List<Order> list = service.findAll();
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
 
 		return ResponseEntity.ok().body(list);
 	}
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Order> findById(@PathVariable Long id) {
-		Order obj = service.findById(id);
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 
