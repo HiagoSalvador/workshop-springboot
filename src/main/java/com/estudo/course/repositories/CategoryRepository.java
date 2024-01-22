274b4f1dc89aea6eb65bdb1cfa012553c3c5bb93
HiagoSalvador <hiago.salvador@hotmail.com>
2024-01-22 13:39:20 -0300

    Category

---
+++ src/main/java/com/estudo/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.estudo.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estudo.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
